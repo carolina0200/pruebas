53144954a476ecfac2942f35158c06ba93f098ce
Carolina <carolinagiraldo@saimyr.com.co>
2020-12-08 20:12:12 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from 'src/environments/environment';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDyhcEOC8v8I0gIV2jx7VJ7ANQIvPmw560',
+    authDomain: 'pruebas-ae73f.firebaseapp.com',
+    databaseURL: 'https://pruebas-ae73f.firebaseio.com',
+    projectId: 'pruebas-ae73f',
+    storageBucket: 'pruebas-ae73f.appspot.com',
+    messagingSenderId: '737951344356',
+    appId: '1:737951344356:web:9f5e07ea488e4c2f0b6ef1',
+    measurementId: 'G-GYET10ZS3M'
+  }
 };
 
 /*
